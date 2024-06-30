b5c161cd61fa82829e515c9379675886eb38378c
Falak Naaz <falaknaaz1104@gmail.com>
2024-06-30 12:01:26 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,233 @@
+let quizData = [
+    {
+      question: "Which author famously wrote their last novel in crayon",
+      options: ["James Joyce", "Jane Austen", "Mark Twain ", "Charles Dickens"],
+      correct: "James Joyce",
+    },
+    {
+      question: "who is the author of malgudi days?",
+      options: ["Kamala Das", "R.K Narayan", "Vyasa", "Mulk Raj"],
+      correct: "R.K Narayan",
+    },
+    {
+      question:
+        "How many lines does a sonnet have?",
+      options: [
+        "10",
+        "12",
+        "14",
+        "20",
+      ],
+      correct: "14",
+    },
+    {
+      question: " which is the wizard prison in harry potter series ?",
+      options: ["Hogwarts", "Azkaban", "Hogsmeade", "Trintardy"],
+      correct: "Azkaban",
+    },
+    {
+      question: "Who is regarded as the father of the english novel?",
+      options: [
+        "Thomas Hardy",
+        "Henry Fielding",
+        "Jane Austen",
+        "Mark Twain",
+      ],
+      correct: "Henry Fielding",
+    },
+    {
+      question: "Who is considered the master of realistic fiction?",
+      options: [
+        "Leo Tolstoy",
+        "Mark Twain",
+        "Oscar Wilde",
+        "Charles Dickens",
+      ],
+      correct: "Leo Tolstoy",
+    },
+    {
+      question: "Which british novelist created the iconic character of James Bond?",
+      options: [
+        "Agatha Christie",
+        "Oscar Wilde",
+        "Mark Twain",
+        "Ian Fleming",
+      ],
+      correct: "Ian Fleming",
+    },
+    {
+      question:
+        "Which novel is considered the first Gothic novel in the English language?",
+      options: [
+        "The Castle of Otranto",
+        "Hieroglyphic Tales",
+        "Selected letters of Horace Walpole",
+        "The Mysterious Mother: A Tragedy",
+      ],
+      correct: "The Castle of Otranto",
+    },
+    {
+      question: "Who wrote the novel 'Pride and Prejudice'?",
+      options: [
+        "Emily Brontë",
+        "Charlotte Brontë",
+        "Jane Austen",
+        "Louisa May Alcott",
+      ],
+      correct: "Jane Austen",
+    },
+  ];
+  
+  const quizContainer = document.querySelector(".quiz-container");
+  const question = document.querySelector(".quiz-container .question");
+  const options = document.querySelector(".quiz-container .options");
+  const nextBtn = document.querySelector(".quiz-container .next-btn");
+  const quizResult = document.querySelector(".quiz-result");
+  const startBtnContainer = document.querySelector(".start-btn-container");
+  const startBtn = document.querySelector(".start-btn-container .start-btn");
+  
+  let questionNumber = 0;
+  let score = 0;
+  const MAX_QUESTIONS = 5;
+  let timerInterval;
+  
+  const shuffleArray = (array) => {
+    return array.slice().sort(() => Math.random() - 0.5);
+  };
+  
+  quizData = shuffleArray(quizData);
+  
+  const resetLocalStorage = () => {
+    for (i = 0; i < MAX_QUESTIONS; i++) {
+      localStorage.removeItem(`userAnswer_${i}`);
+    }
+  };
+  
+  resetLocalStorage();
+  
+  const checkAnswer = (e) => {
+    let userAnswer = e.target.textContent;
+    if (userAnswer === quizData[questionNumber].correct) {
+      score++;
+      e.target.classList.add("correct");
+    } else {
+      e.target.classList.add("incorrect");
+    }
+  
+    localStorage.setItem(`userAnswer_${questionNumber}`, userAnswer);
+  
+    let allOptions = document.querySelectorAll(".quiz-container .option");
+    allOptions.forEach((o) => {
+      o.classList.add("disabled");
+    });
+  };
+  
+  const createQuestion = () => {
+    clearInterval(timerInterval);
+  
+    let secondsLeft = 9;
+    const timerDisplay = document.querySelector(".quiz-container .timer");
+    timerDisplay.classList.remove("danger");
+  
+    timerDisplay.textContent = `Time Left: 10 seconds`;
+  
+    timerInterval = setInterval(() => {
+      timerDisplay.textContent = `Time Left: ${secondsLeft
+        .toString()
+        .padStart(2, "0")} seconds`;
+      secondsLeft--;
+  
+      if (secondsLeft < 3) {
+        timerDisplay.classList.add("danger");
+      }
+  
+      if (secondsLeft < 0) {
+        clearInterval(timerInterval);
+        displayNextQuestion();
+      }
+    }, 1000);
+  
+    options.innerHTML = "";
+    question.innerHTML = `<span class='question-number'>${
+      questionNumber + 1
+    }/${MAX_QUESTIONS}</span>${quizData[questionNumber].question}`;
+  
+    const shuffledOptions = shuffleArray(quizData[questionNumber].options);
+  
+    shuffledOptions.forEach((o) => {
+      const option = document.createElement("button");
+      option.classList.add("option");
+      option.innerHTML = o;
+      option.addEventListener("click", (e) => {
+        checkAnswer(e);
+      });
+      options.appendChild(option);
+    });
+  };
+  
+  const retakeQuiz = () => {
+    questionNumber = 0;
+    score = 0;
+    quizData = shuffleArray(quizData);
+    resetLocalStorage();
+  
+    createQuestion();
+    quizResult.style.display = "none";
+    quizContainer.style.display = "block";
+  };
+  
+  const displayQuizResult = () => {
+    quizResult.style.display = "flex";
+    quizContainer.style.display = "none";
+    quizResult.innerHTML = "";
+  
+    const resultHeading = document.createElement("h2");
+    resultHeading.innerHTML = `You have scored ${score} out of ${MAX_QUESTIONS}.`;
+    quizResult.appendChild(resultHeading);
+  
+    for (let i = 0; i < MAX_QUESTIONS; i++) {
+      const resultItem = document.createElement("div");
+      resultItem.classList.add("question-container");
+  
+      const userAnswer = localStorage.getItem(`userAnswer_${i}`);
+      const correctAnswer = quizData[i].correct;
+  
+      let answeredCorrectly = userAnswer === correctAnswer;
+  
+      if (!answeredCorrectly) {
+        resultItem.classList.add("incorrect");
+      }
+  
+      resultItem.innerHTML = `<div class="question">Question ${i + 1}: ${
+        quizData[i].question
+      }</div>
+      <div class="user-answer">Your answer: ${userAnswer || "Not Answered"}</div>
+      <div class="correct-answer">Correct answer: ${correctAnswer}</div>`;
+  
+      quizResult.appendChild(resultItem);
+    }
+  
+    const retakeBtn = document.createElement("button");
+    retakeBtn.classList.add("retake-btn");
+    retakeBtn.innerHTML = "Retake Quiz";
+    retakeBtn.addEventListener("click", retakeQuiz);
+    quizResult.appendChild(retakeBtn);
+  };
+  
+  const displayNextQuestion = () => {
+    if (questionNumber >= MAX_QUESTIONS - 1) {
+      displayQuizResult();
+      return;
+    }
+  
+    questionNumber++;
+    createQuestion();
+  };
+  
+  nextBtn.addEventListener("click", displayNextQuestion);
+  
+  startBtn.addEventListener("click", () => {
+    startBtnContainer.style.display = "none";
+    quizContainer.style.display = "block";
+    createQuestion();
+  });
